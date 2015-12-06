3c1c0ef5b8d3bb01317b88f1e79478d947ef0884
bdstein33 <bdstein33@gmail.com>
2015-12-06 10:13:58 -0800

    Add Login route

--- components/Html.js
+++ components/Html.js
@@ -1,7 +1,3 @@
-/**
- * Copyright 2014, Yahoo! Inc.
- * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
- */
 import React from 'react';
 import ApplicationStore from '../stores/ApplicationStore';
 
--- configs/routes.js
+++ configs/routes.js
@@ -1,12 +1,22 @@
 export default {
-    home: {
-        path: '/',
-        method: 'get',
-        handler: require('../components/Home'),
-        label: 'Home',
-        action: (context, payload, done) => {
-            context.dispatch('UPDATE_PAGE_TITLE', { pageTitle: 'Home | flux-examples | routing' });
-            done();
-        }
+  home: {
+    path: '/',
+    method: 'get',
+    handler: require('../components/Home'),
+    label: 'Home',
+    action: (context, payload, done) => {
+      context.dispatch('UPDATE_PAGE_TITLE', { pageTitle: 'Home' });
+      done();
     }
+  },
+  login: {
+    path: '/login',
+    method: 'get',
+    handler: require('../components/Login'),
+    label: 'Home',
+    action: (context, payload, done) => {
+      context.dispatch('UPDATE_PAGE_TITLE', { pageTitle: 'Sign In' });
+      done();
+    }
+  }
 };
